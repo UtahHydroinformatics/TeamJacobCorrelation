8b24ad65d6b0a9b0496cb8aba5a9e89cfd1079e2
thomasbalkcom <33735395+thomasbalkcom@users.noreply.github.com>
2017-11-28 15:21:08 -0700

    Add files via upload

---
+++ server.R
@@ -0,0 +1,13 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+
+shinyServer(function(input, output) {
+
+  
+
+})
---
+++ ui.R
@@ -0,0 +1,25 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Climate Data Application"),
+
+  # Sidebar with user input controls
+  sidebarLayout( 
+    sidebarPanel(
+     
+    ),
+
+    # Show outputs, text, etc. in the main panel
+    mainPanel(
+      
+    )
+  )
+))
